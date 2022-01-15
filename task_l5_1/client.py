fc59b6ccb79f18f40878712e0dbb9c97bf0aa15f
sasha-hungry <argus2005@inbox.ru>
2022-01-15 23:38:43 +0300

    done syslog configuration for client and started add logging on client's script

---
+++ task_l5_1/client.py
@@ -0,0 +1,68 @@
+"""Программа-клиент"""
+
+import sys
+import json
+import socket
+import time
+import logging
+import log.config_client_log
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message, get_address_port
+
+
+client_logger = logging.getLogger('client')
+
+def create_presence(account_name='Guest'):
+    '''
+    Функция генерирует запрос о присутствии клиента
+    :param account_name:
+    :return:
+    '''
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    client_logger.debug(f'сгенерирован запрос PRESENCE для username: {account_name}')
+    return out
+
+
+def process_ans(message):
+    '''
+    Функция разбирает ответ сервера
+    :param message:
+    :return:
+    '''
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    '''Загружаем параметы коммандной строки'''
+    # client.py -a 192.168.0.100 -p 6666 
+
+    # Инициализация сокета и обмен
+    address_port = get_address_port()
+    server_address = address_port[0]
+    server_port = address_port[1]
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ task_l5_1/common/utils.py
@@ -0,0 +1,74 @@
+"""Утилиты"""
+
+import json
+import sys
+from common.variables import MAX_PACKAGE_LENGTH, ENCODING, DEFAULT_PORT
+
+def get_address_port():
+    '''
+    Считывает параметры командной строки, если нет параметров 
+    использует данные по умолчанию (localhost:7777).
+    Пример команды запуска:
+    server.py -a 192.168.0.100 -p 6666
+    '''
+
+    try:
+        if '-p' in sys.argv:
+            port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            port = DEFAULT_PORT
+        if port < 1024 or port > 65535:
+            raise ValueError
+    except IndexError:
+        print('После параметра -\'p\' необходимо указать номер порта.')
+        sys.exit(1)
+    except ValueError:
+        print(
+            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+
+    try:
+        if '-a' in sys.argv:
+            address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            address = ''
+
+    except IndexError:
+        print(
+            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+        sys.exit(1)
+    
+    address_port = [address, port]
+    return address_port
+
+def get_message(client):
+    '''
+    Утилита приёма и декодирования сообщения
+    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
+    '''
+
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise ValueError
+    raise ValueError
+
+
+def send_message(sock, message):
+    '''
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    '''
+
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
